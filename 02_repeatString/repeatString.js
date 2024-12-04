f5ed51128248befac9d4d1168d25478f1efd47b2
mileshinch <miles.hinchliffe@levellingup.gov.uk>
2024-12-04 14:34:59 +0000

    Complete section 1

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
+let repititions = 3;
+let string = '';
 
+
+const repeatString = function(string,repititions) {
+    let returnedString = '';
+    if (repititions < 0) {
+        returnedString = 'ERROR';
+    } else {
+        for (let i = 1; i <= repititions; i++) {
+            returnedString = returnedString.concat(string);
+        }
+    }
+    return returnedString;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,22 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    // create an empty array to store the string characters
+    let array = [];
+    // push each character from string into an empty array
+    for (character of string) {
+        array.push(character);
+    }
 
+    // reverse the array
+    array = array.reverse();
+    
+    // create an empty string to store the result
+    let reversedString = '';
+
+    // take each element of the array and append it to reversedString
+    for (let i = 0; i <= array.length - 1; i++) {
+        reversedString = reversedString + array[i];
+    }
+    return reversedString;
 };
 
 // Do not edit below this line
