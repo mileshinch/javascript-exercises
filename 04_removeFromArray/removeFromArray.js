5c12747a90327186567bab71d5c87b71323eb7d0
mileshinch <miles.hinchliffe@levellingup.gov.uk>
2024-12-05 15:42:31 +0000

    Work on solution for exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+
+const removeFromArray = function(arrayOfNumbers = [0,1,2,3], number = 3) {
+    //define variables
+    
+    // remove number from arrayOfNumbers
+        // get the index of number
+        let index = arrayOfNumbers.indexOf(number);
+        // use splice to remove that element
+        arrayOfNumbers.splice(index,1);
+        return arrayOfNumbers;
 };
 
 // Do not edit below this line
