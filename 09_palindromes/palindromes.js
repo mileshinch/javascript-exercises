7514857ff6f0e580c8dd497c1680f344786a7555
Miles Hinchliffe <miles.hinchliffe@levellingup.gov.uk>
2025-01-27 22:00:04 +0000

    I suspect my function always returns true regardless

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,21 @@
-const palindromes = function () {
+const palindromes = function (str) {
+     // remove punctionation
+     str = str.replace(/[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g, '');
+     // convert to lower case
+     str = str.toLowerCase()
+     // loop through string does the first letter === last letter
+    for (let i = 0; i < str.length/2; i++) {
+       
 
-};
+        // does the first letter equal the last letter
+        if (!(str[i] === str[str.length-i])) {
+            break;
+            return false
+        }
+    }
+    return true
+    }
+;
 
 // Do not edit below this line
 module.exports = palindromes;
