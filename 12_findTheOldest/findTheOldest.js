24cedf553d4087760bf7b9af9731e45784591268
mileshinch <miles.hinchliffe@levellingup.gov.uk>
2025-01-31 13:06:50 +0000

    Finally complete this tricky reduce exercise!!!

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,23 @@
-const findTheOldest = function() {
 
+
+const findTheOldest = function(people) {
+    const currentYear = 2025;
+    
+    const oldestPerson = people.reduce((acc,curr) => 
+    //curr.yearOfDeath - curr.yearOfBirth > acc.yearOfDeath - acc.yearOfBirth
+    //? curr : acc
+
+    // Handle missing 'yearOfDeath' by using the current year
+    {
+    const currAge = curr.yearOfDeath ? curr.yearOfDeath - curr.yearOfBirth : currentYear - curr.yearOfBirth;
+    const accAge = acc.yearOfDeath ? acc.yearOfDeath - acc.yearOfBirth : currentYear - acc.yearOfBirth;
+    
+    // Compare ages and return the person with the greater age
+    return currAge > accAge ? curr : acc;
+
+    }, people[0])
+    
+    return oldestPerson
 };
 
 // Do not edit below this line
